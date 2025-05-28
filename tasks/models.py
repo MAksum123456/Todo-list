83908971e71915fb6e4fa6d7fb1379518068a82f
Max <smolinskijmaksim1@gmail.com>
2025-05-28 19:22:43 +0100

    Add initial models: Task and Tag

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True,related_name="tasks")
+
+    class Meta:
+        ordering = ["is_completed", "-date"]
+
+    def __str__(self):
+        return f"{self.content[:30]}{'...' if len(self.content) > 30 else ''}"
